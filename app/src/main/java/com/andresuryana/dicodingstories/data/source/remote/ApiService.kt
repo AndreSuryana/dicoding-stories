807cf0ca760942e2987863659c3ced580ee766b5
AndreSuryana <Andresuryana17@gmail.com>
2023-06-22 08:58:11 +0700

    remove slash in API path

--- app/src/main/java/com/andresuryana/dicodingstories/data/source/remote/ApiService.kt
+++ app/src/main/java/com/andresuryana/dicodingstories/data/source/remote/ApiService.kt
@@ -7,6 +7,8 @@ import com.andresuryana.dicodingstories.util.Constants.STORIES_PAGE_SIZE
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.Response
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
 import retrofit2.http.Multipart
 import retrofit2.http.POST
@@ -15,22 +17,31 @@ import retrofit2.http.Query
 
 interface ApiService {
 
-    @POST("/register")
-    fun register(name: String, email: String, password: String): Response<Wrapper<Nothing>>
+    @FormUrlEncoded
+    @POST("register")
+    suspend fun register(
+        @Field("name") name: String,
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): Response<Wrapper<Nothing>>
 
-    @POST("/login")
-    fun login(email: String, password: String): Response<Wrapper<User>>
+    @FormUrlEncoded
+    @POST("login")
+    suspend fun login(
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): Response<Wrapper<User>>
 
-    @GET("/stories")
-    fun getStories(
+    @GET("stories")
+    suspend fun getStories(
         @Query("page") page: Int = 1,
         @Query("size") size: Int = STORIES_PAGE_SIZE,
         @Query("location") location: Int = 0
     ): Response<Wrapper<List<Story>>>
 
     @Multipart
-    @POST("/stories")
-    fun addNewStory(
+    @POST("stories")
+    suspend fun addNewStory(
         @Part("photo") photo: MultipartBody.Part,
         @Part("description") description: RequestBody
     ): Response<Wrapper<Nothing>>
