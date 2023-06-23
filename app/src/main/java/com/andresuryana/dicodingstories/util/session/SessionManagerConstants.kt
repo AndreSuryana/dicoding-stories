9fbac7fdb4915583ac1805d64e7494e46f79b413
AndreSuryana <Andresuryana17@gmail.com>
2023-06-23 09:31:47 +0700

    remove session manager class

--- app/src/main/java/com/andresuryana/dicodingstories/util/session/SessionManagerConstants.kt
+++
@@ -1,9 +0,0 @@
-package com.andresuryana.dicodingstories.util.session
-
-object SessionManagerConstants {
-    const val PREFS_NAME = "DicodingStoriesPrefs"
-    const val KEY_IS_LOGGED_IN = "isLoggedIn"
-    const val KEY_USER_ID = "userId"
-    const val KEY_USER_NAME = "userName"
-    const val KEY_USER_TOKEN = "authToken"
-}
